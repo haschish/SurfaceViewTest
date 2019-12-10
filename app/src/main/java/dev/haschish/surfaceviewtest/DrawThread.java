286c045f4d16bde7ca2ed149b1e090d30f7215b9
haschish <haschish@gmail.com>
2019-12-10 15:41:39 +0300

    added painting of smile

--- app/src/main/java/dev/haschish/surfaceviewtest/DrawThread.java
+++ app/src/main/java/dev/haschish/surfaceviewtest/DrawThread.java
@@ -1,14 +1,31 @@
 package dev.haschish.surfaceviewtest;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 
 public class DrawThread extends Thread {
     private SurfaceHolder surfaceHolder;
     private volatile boolean running = true;
 
+    private Paint backPaint = new Paint();
+    private Bitmap bitmap;
+    private int towardPointX;
+    private int towardPointY;
+
+    {
+        backPaint.setColor(Color.BLUE);
+        backPaint.setStyle(Paint.Style.FILL);
+    }
+
+
+
     public DrawThread(Context context, SurfaceHolder surfaceHolder) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.smile);
         this.surfaceHolder = surfaceHolder;
     }
 
@@ -16,13 +33,36 @@ public class DrawThread extends Thread {
         running = false;
     }
 
+    public void setTowardPoint(int x, int y) {
+        towardPointX = x;
+        towardPointY = y;
+    }
+
     @Override
     public void run() {
+        int smileX = 0;
+        int smileY = 0;
+
         while(running) {
             Canvas canvas = surfaceHolder.lockCanvas();
 
             if (canvas != null) {
                 try {
+                    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backPaint);
+                    canvas.drawBitmap(bitmap, smileX, smileY, backPaint);
+
+                    if (smileX + bitmap.getWidth() / 2 < towardPointX) {
+                        smileX += 10;
+                    }
+                    if (smileX + bitmap.getWidth() / 2 > towardPointX) {
+                        smileX -= 10;
+                    }
+                    if (smileY + bitmap.getHeight() / 2 < towardPointY) {
+                        smileY += 10;
+                    }
+                    if (smileY + bitmap.getHeight() / 2 > towardPointY) {
+                        smileY -= 10;
+                    }
                     //рисование на canvas
                 } finally {
                     surfaceHolder.unlockCanvasAndPost(canvas);
--- app/src/main/java/dev/haschish/surfaceviewtest/DrawView.java
+++ app/src/main/java/dev/haschish/surfaceviewtest/DrawView.java
@@ -1,6 +1,7 @@
 package dev.haschish.surfaceviewtest;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -35,5 +36,11 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                 //
             }
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        drawThread.setTowardPoint((int)event.getX(), (int)event.getY());
+        return super.onTouchEvent(event);
     }
 }
